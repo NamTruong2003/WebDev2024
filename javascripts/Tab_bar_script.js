256363e1d905ef25ff22421f5490771417dcf82a
unknown <hung200368@gmail.com>
2024-10-07 11:24:52 +0700

    added dashboard link and fix stuff

---
+++ javascripts/Tab_bar_script.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let expand_btns = document.querySelectorAll(".bx-chevron-down");
+    expand_btns.forEach(btn => {
+        btn.addEventListener("click", (e) => {
+            let parentLi = e.target.closest('li');
+            parentLi.classList.toggle("showMenu");
+        });
+    });
+
+    let sidebar = document.querySelector(".sidebar");
+    let sidebarBtn = document.querySelector(".bx-menu");
+    console.log(sidebarBtn);
+    sidebarBtn.addEventListener("click", () => {
+        sidebar.classList.toggle("close");
+    });
+});
